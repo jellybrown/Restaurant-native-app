01f7b4c0ff75ca5c4dfda4f03d4d99ae367a97c6
yujin mac <kimyu28jin@gmail.com>
2021-08-14 15:58:12 +0900

    Feat : 검색기능 추가

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [result, setResult] = useState([]);
+  const [error, setError] = useState('');
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          term: searchTerm,
+          limit: 50,
+          location: 'san jose',
+        },
+      });
+      setResult(response.data.businesses);
+    } catch (err) {
+      setError('Something went wrong..');
+    }
+  };
+
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
+
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={setTerm}
-        onTermSubmit={() => console.log('제출')}
+        onTermSubmit={() => searchApi(term)}
       />
-      <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      {error ? <Text>{error}</Text> : null}
+      <Text>We have found {result.length} results</Text>
     </View>
   );
 };
