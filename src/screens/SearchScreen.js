e171ac825bfe7f7017dd66832cbeba86f75e6234
yujin mac <kimyu28jin@gmail.com>
2021-08-17 16:29:29 +0900

    Feat : ResultList 컴포넌트 추가

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ResultList from '../components/ResultList';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [searchApi, result, errorMessage] = useResults();
+  const [searchApi, results, errorMessage] = useResults();
 
+  const filterByPrice = (price) => {
+    return results.filter((result) => result.price === price);
+  };
   return (
     <View>
       <SearchBar
@@ -16,7 +20,10 @@ const SearchScreen = () => {
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {result.length} results</Text>
+      <Text>We have found {results.length} results</Text>
+      <ResultList results={filterByPrice('$')} title="Cost Effective" />
+      <ResultList results={filterByPrice('$$')} title="Bit Pricer" />
+      <ResultList results={filterByPrice('$$$')} title="Big Spender!" />
     </View>
   );
 };
